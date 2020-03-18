8bce3596139109fef2fafd52e0af66d0be890b0b
Macierz-maker <s18239@pjwstk.edu.pl>
2020-03-18 21:21:05 +0100

    Yey, commit!

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw3___v2.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
