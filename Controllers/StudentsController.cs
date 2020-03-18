263bda0730c8306fee4dcc13f05e7f29e0b486d1
Macierz-maker <s18239@pjwstk.edu.pl>
2020-03-18 21:45:32 +0100

    Yey, commit! It worked without HttpGet, though I've added it

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -5,10 +5,13 @@ namespace cw3___v2.Controllers
     [ApiController]
     [Route("api/students")]
     public class StudentsController : ControllerBase
-    {
+    { 
+        [HttpGet]
         public string GetStudent()
         {
             return "Kowalski, Malewski, Andrzejewski";
         }
+        
+        // public 
     }
 }
