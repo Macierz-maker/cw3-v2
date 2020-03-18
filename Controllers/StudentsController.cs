cd9be70bcd0fe1da3190fa37a06175cadd13b438
Macierz-maker <s18239@pjwstk.edu.pl>
2020-03-18 22:20:40 +0100

    Updated GetStudent() method

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -5,13 +5,32 @@ namespace cw3___v2.Controllers
     [ApiController]
     [Route("api/students")]
     public class StudentsController : ControllerBase
-    { 
+    {
         [HttpGet]
         public string GetStudent()
         {
             return "Kowalski, Malewski, Andrzejewski";
         }
         
-        // public 
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
     }
 }
