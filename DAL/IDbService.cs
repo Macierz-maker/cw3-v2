8b616128a15d04379e8a050582157785581412b2
Macierz-maker <61716605+Macierz-maker@users.noreply.github.com>
2020-03-19 22:46:03 +0100

    Will it update, this is the question

--- DAL/IDbService.cs
+++ DAL/IDbService.cs
@@ -1,7 +1,10 @@
-﻿namespace cw3___v2.DAL
+﻿using System.Collections.Generic;
+using cw3___v2.Models;
+
+namespace cw3___v2.DAL
 {
     public interface IDbService
     {
-        
+        public IEnumerable<Student> GetStudents();
     }
 }
